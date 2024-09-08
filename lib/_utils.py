44d6f34fac761674f665d487ebb17d43a9f96ccb
Sangboom <saturnbum@naver.com>
2024-09-08 20:46:41 +0900

    docs: comment out print

--- lib/_utils.py
+++ lib/_utils.py
@@ -56,12 +56,12 @@ class _LAVTOneSimpleDecode(nn.Module):
         else:
             l_feats = self.text_encoder(text, attention_mask=l_mask)[0]  # (6, 10, 768)
 
-        print('l_feats:', l_feats.shape)
+        # print('l_feats:', l_feats.shape)
         l_feats = l_feats.permute(0, 2, 1)  # (B, 768, N_l) to make Conv1d happy
         l_mask = l_mask.unsqueeze(dim=-1)  # (batch, N_l, 1)
         ##########################
-        print('l_feats:', l_feats.shape)
-        print('l_mask:', l_mask.shape)
+        # print('l_feats:', l_feats.shape)
+        # print('l_mask:', l_mask.shape)
         features = self.backbone(x, l_feats, l_mask)
         x_c1, x_c2, x_c3, x_c4 = features
         x = self.classifier(x_c4, x_c3, x_c2, x_c1)
